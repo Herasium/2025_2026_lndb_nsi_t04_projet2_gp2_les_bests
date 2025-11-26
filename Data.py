972467083cef3da0613749908beb4ae2d060699d
Arkunir <abricotpomme1234@gmail.com>
2025-11-26 09:55:15 +0100

    Update Data.py

--- Data.py
+++ Data.py
@@ -85,6 +85,19 @@ def comp_impa(input_a, input_b):
 
 def comp_impb(input_a, input_b):
     return comp_nor(input_a, comp_not(input_b))
+            
+def full_adder(input_a, input_b, carry_in):
+    sum = comp_xor(input_a, input_b)
+    sum = comp_xor(sum, carry_in)
+    carry_out = comp_and(input_a, input_b)
+    carry_out = comp_or(carry_out, comp_and(carry_in, comp_xor(input_a, input_b)))
+    return sum, carry_out
+
+def calculator(input_a, input_b, carry_in, bits):
+    while bits > 0:
+        bits -= 1
+        result = (2**(bits-1))*(1*(full_adder(input_a, input_b, carry_in)))
+    return result
 
 def truth_table():
     return comp_and(a,a), comp_and(a,b), comp_and(b,a), comp_and(b,b),\
@@ -95,10 +108,13 @@ def truth_table():
         comp_nor(a,a), comp_nor(a,b), comp_nor(b,a), comp_nor(b,b),\
         comp_nxor(a,a), comp_nxor(a,b), comp_nxor(b,a), comp_nxor(b,b),\
         comp_impa(a,a), comp_impa(a,b), comp_impa(b,a), comp_impa(b,b),\
-        comp_impb(a,a), comp_impb(a,b), comp_impb(b,a), comp_impb(b,b)\
-            
+        comp_impb(a,a), comp_impb(a,b), comp_impb(b,a), comp_impb(b,b),\
+        full_adder(b,b,b), full_adder(b,b,a), full_adder(b,a,b),\
+        full_adder(a,b,b), full_adder(b,a,a), full_adder(a,b,a),\
+        full_adder(a,a,b), full_adder(a,a,a)
 
 a = True
 b = False
 
-print(comp_impa(a, a))
+# print(truth_table())
+print(calculator(a, a, a, 2))
